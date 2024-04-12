fd1b9348abab7d5e8bbfe2a5d5f7117b4e03c9dc
Fabian Rosario <fabianraul1210@gmail.com>
2024-04-12 19:15:01 -0400

    Finish Nfc

--- src/main/java/Team_2_2_2/NFCApp/controllers/NfcController.java
+++ src/main/java/Team_2_2_2/NFCApp/controllers/NfcController.java
@@ -1,8 +1,6 @@
 package Team_2_2_2.NFCApp.controllers;
 
-import Team_2_2_2.NFCApp.entities.NfcEntity;
 import Team_2_2_2.NFCApp.services.NfcService;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +15,7 @@ public class NfcController {
         this.nfcService = nfcService;
     }
 
-    @PostMapping("/")
+    @PostMapping("/add")
     public ResponseEntity<String> addNfc(@RequestParam String nfcId) {
         nfcService.addNfc(nfcId);
 
--- src/main/java/Team_2_2_2/NFCApp/services/NfcService.java
+++ src/main/java/Team_2_2_2/NFCApp/services/NfcService.java
@@ -1,22 +1,18 @@
 package Team_2_2_2.NFCApp.services;
 
 import Team_2_2_2.NFCApp.entities.NfcEntity;
-import Team_2_2_2.NFCApp.entities.ObjectEntity;
 import Team_2_2_2.NFCApp.repositories.NfcRepository;
 import Team_2_2_2.NFCApp.repositories.ObjectRepository;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NfcService {
     private final NfcRepository nfcRepository;
-    private final ObjectRepository objectRepository;
 
     @Autowired
     public NfcService(NfcRepository nfcRepository, ObjectRepository objectRepository) {
         this.nfcRepository = nfcRepository;
-        this.objectRepository = objectRepository;
     }
 
     public void addNfc(String nfcId) {
