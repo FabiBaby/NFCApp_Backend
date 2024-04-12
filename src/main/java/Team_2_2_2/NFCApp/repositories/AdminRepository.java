4d8a4763ad667f4f4da1c30bb499b512bd54e14f
Fabian Rosario <fabianraul1210@gmail.com>
2024-04-12 14:31:07 -0400

    Fix AdminRepository

--- src/main/java/Team_2_2_2/NFCApp/repositories/AdminRepository.java
+++ src/main/java/Team_2_2_2/NFCApp/repositories/AdminRepository.java
@@ -4,8 +4,5 @@ import Team_2_2_2.NFCApp.entities.AdminEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<AdminEntity, Long> {
-    //Find an Admin entity by its ID
-    AdminEntity findByAdminId(int AdminId);
-
     AdminEntity findByUsername(String username);
 }
