e73cd58dc25c6352cb2b65b542b5a53e3e688446
Fabian Rosario <fabianraul1210@gmail.com>
2024-04-22 16:22:09 -0400

    Made AdminDto and ObjectDto

---
+++ src/main/java/Team_2_2_2/NFCApp/dto/ObjectDto.java
@@ -0,0 +1,29 @@
+package Team_2_2_2.NFCApp.dto;
+
+public class ObjectDto {
+    private String objectName;
+    private String objectDesc;
+    private String objectLocation;
+    private String NfcId;
+    private Long objectId;
+
+    public String getObjectName() {
+        return objectName;
+    }
+
+    public String getObjectDesc() {
+        return objectDesc;
+    }
+
+    public String getObjectLocation() {
+        return objectLocation;
+    }
+
+    public Long getObjectId() {
+        return objectId;
+    }
+
+    public String getNfcId() {
+        return NfcId;
+    }
+}
