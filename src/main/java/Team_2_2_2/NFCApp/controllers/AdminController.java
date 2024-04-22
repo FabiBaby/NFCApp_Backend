d1a083b8fcb762f95b2329361839851dd6ccefcc
Fabian Rosario <fabianraul1210@gmail.com>
2024-04-22 14:51:55 -0400

    Revoked changes in LoginResponse

--- src/main/java/Team_2_2_2/NFCApp/controllers/AdminController.java
+++ src/main/java/Team_2_2_2/NFCApp/controllers/AdminController.java
@@ -140,5 +140,22 @@ public class AdminController {
             this.success = success;
             this.message = message;
         }
+
+        // Getters and setters
+        public boolean isSuccess() {
+            return success;
+        }
+
+        public void setSuccess(boolean success) {
+            this.success = success;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
     }
 }
