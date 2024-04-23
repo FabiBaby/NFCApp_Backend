bca7dc168cec28d9e701fcf9876c4607cc0907b5
Fabian Rosario <fabianraul1210@gmail.com>
2024-04-22 20:04:24 -0400

    Remove redundant code

--- src/main/java/Team_2_2_2/NFCApp/controllers/AdminController.java
+++ src/main/java/Team_2_2_2/NFCApp/controllers/AdminController.java
@@ -61,34 +61,6 @@ public class AdminController {
         return ResponseEntity.status(HttpStatus.OK).body(allObjectEntities);
     }
 
-    public static class AssignNfcRequest {
-        private ObjectDto objectDto;
-        private String nfcUID;
-
-        // Constructors
-        public AssignNfcRequest(ObjectDto objectDto, String nfcUid) {
-            this.objectDto = objectDto;
-            this.nfcUID = nfcUID;
-        }
-
-        // Getters and setters
-        public ObjectDto getObjectEntity() {
-            return objectDto;
-        }
-
-        public void setObjectEntity(ObjectDto objectDto) {
-            this.objectDto = objectDto;
-        }
-
-        public String getNfcUid() {
-            return nfcUID;
-        }
-
-        public void setNfcUid(String nfcUID) {
-            this.nfcUID = nfcUID;
-        }
-    }
-
     public static class ObjectDto {
         private String objectName;
         private String objectDesc;
